5466e185428c9156e38ef5140c5b96105dabe4f8
sxy-sun <sunxycn98@gmail.com>
2024-12-05 18:38:15 -0500

    added imu test

--- mbot_sys_cli/mbot_status/mbot_status.py
+++ mbot_sys_cli/mbot_status/mbot_status.py
@@ -4,41 +4,81 @@ import lcm
 import time
 import sys
 import subprocess
+from mbot_lcm_msgs.mbot_analog_t import mbot_analog_t
+from mbot_lcm_msgs.mbot_imu_t import mbot_imu_t
+import threading
+
+# Define variables
+RATE = 1
 
 # Command-line argument parsing
 parser = argparse.ArgumentParser(description="MBot Status")
 parser.add_argument("--topic", type=str, help="Topic to monitor (default: all topics)")
 parser.add_argument("--continuous", action="store_true", help="Continue monitoring status (until Ctrl+C)")
+parser.add_argument("--verbose", action="store_true", help="Give details (only valid if --topic is specified)")
 args = parser.parse_args()
 
-class MBotBatteryStatus:
-    def __init__(self):
-        # Set up LCM if available
-        self.lc = lcm.LCM("udpm://239.255.76.67:7667?ttl=0")
-        self.battery_voltage = -1
-        self.mbot_lcm_installed = self.check_mbot_lcm_installed()
-        if self.mbot_lcm_installed:
-            self.lc.subscribe("MBOT_ANALOG_IN", self.battery_info_callback)
+# Custom validation
+if args.verbose and not args.topic:
+    parser.error("--verbose can only be used if --topic is specified")
 
-    def check_mbot_lcm_installed(self):
-        try:
-            from mbot_lcm_msgs.mbot_analog_t import mbot_analog_t
-            self.mbot_analog_t = mbot_analog_t
-            return True
-        except ImportError:
-            self.battery_voltage = -2  # error code
-            print("Error: mbot_lcm_msgs not installed.")
-            return False
-          
-    def battery_info_callback(self, channel, data):
-        battery_info = self.mbot_analog_t.decode(data)
-        self.battery_voltage = battery_info.volts[3]
-    
-    def get_battery_status(self):
-        if self.lc:
+
+class LCMFetch:
+    def __init__(self):
+        self.battery_voltage = -1
+        self.imu_readings = []
+        self.initialized = {"battery": False, "imu": False}
+
+        self.lc = lcm.LCM("udpm://239.255.76.67:7667?ttl=0")
+
+        # subscriber
+        self.lc.subscribe("MBOT_ANALOG_IN", self.battery_info_callback)
+        self.lc.subscribe("MBOT_IMU", self.imu_info_callback)
+
+        # Start a thread to handle LCM messages continuously
+        self.lcm_thread = threading.Thread(target=self.lcm_handler, daemon=True)
+        self.lcm_thread.start()
+
+    def lcm_handler(self):
+        while True:
             # Wait for a message for up to 1 second
+            past_time = time.time()
             self.lc.handle_timeout(1000)
-        return self.battery_voltage
+            if time.time() - past_time > 1:
+                self.reset_variables()
+
+    def battery_info_callback(self, channel, data):
+        battery_info = mbot_analog_t.decode(data)
+        self.battery_voltage = battery_info.volts[3]
+        self.initialized["battery"] = True
+
+    def imu_info_callback(self, channel, data):
+        imu_reading = mbot_imu_t.decode(data)
+        roll, pitch, yaw = imu_reading.angles_rpy
+        self.imu_readings = [roll, pitch, yaw]
+        self.initialized["imu"] = True
+
+    def imu_test(self):
+        if self.imu_readings != []:
+            return any(element > 0.0 for element in self.imu_readings)
+        return False
+
+    def get_status(self):
+        imu_test = "Pass" if self.imu_test() else "Fail"
+
+        status_dict = {
+            "battery": self.battery_voltage,
+            "imu_test": imu_test
+        }
+        return status_dict
+
+    def is_initialized(self):
+        # Check if all necessary data has been received
+        return all(self.initialized.values())
+
+    def reset_variables(self):
+        self.battery_voltage = -1
+        self.imu_readings = []
 
 def get_temperature():
     try:
@@ -49,20 +89,66 @@ def get_temperature():
     except Exception as e:
         print(f"Error reading temperature: {e}")
         return -1
-        
+    
+def print_battery(battery_voltage):
+    print(f"{'Battery Voltage:':<20} {battery_voltage:>10.2f} V")
+
+def print_temperature(temperature):
+    print(f"{'Temperature:':<20} {temperature:>10.2f} C")
+
+def print_imu_test(imu_test):
+    # Align the text first, then apply color coding
+    status_text = f"{imu_test:>10}"
+    if imu_test == "Pass":
+        imu_status_colored = f"\033[92m{status_text}\033[0m"  # Green text
+    else:
+        imu_status_colored = f"\033[91m{status_text}\033[0m"  # Red text
+    print(f"{'IMU Test:':<20} {imu_status_colored}")
+
 if __name__ == "__main__":
-    mbot_battery_status = MBotBatteryStatus()
-    battery_voltage = mbot_battery_status.get_battery_status()
-    temperature = get_temperature()
+    lcm_fetched_status = LCMFetch()
+
+    # Wait until the LCM messages are received
+    past_time = time.time()
+    while not lcm_fetched_status.is_initialized():
+        time.sleep(0.1)
+        if time.time() - past_time > 1:
+            break 
 
     if args.topic:
         if args.topic == "battery":
-            print(f"Battery Voltage: {battery_voltage:.2f} V")
-            if args.continuous:
-                while True:
-                    print("\033[H\033[J", end="")  # Clear the screen
-                    battery_voltage = mbot_battery_status.get_battery_status()
-                    print(f"Battery Voltage: {battery_voltage:.2f} V")
+            while True:
+                status_dict = lcm_fetched_status.get_status()
+                print_battery(status_dict['battery'])
+                if not args.continuous:
+                    break
+                time.sleep(1 / RATE)
+        elif args.topic == "temperature":
+            while True:
+                temperature = get_temperature()
+                print_temperature(temperature)
+                if not args.continuous:
+                    break
+                time.sleep(1 / RATE)
+        elif args.topic == "test":
+            while True:
+                status_dict = lcm_fetched_status.get_status()
+                print_imu_test(status_dict['imu_test'])
+                if not args.continuous:
+                    break
+                print("\033[H\033[J", end="")  # Clear the screen
+                time.sleep(1 / RATE)
     else:
-        print(f"Battery Voltage: {battery_voltage:.2f} V")
-        print(f"Temperature: {temperature:.2f} °C")
+        while True:
+            status_dict = lcm_fetched_status.get_status()
+            temperature = get_temperature()
+
+            # Print the status information
+            print_battery(status_dict['battery'])
+            print_temperature(temperature)
+            print_imu_test(status_dict['imu_test'])
+
+            if not args.continuous:
+                break
+            print("\033[H\033[J", end="")  # Clear the screen
+            time.sleep(1 / RATE)
